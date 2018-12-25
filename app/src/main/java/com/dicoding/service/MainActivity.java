18406627e32acbda54d779bc1762ecd5f9fe25f3
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2018-12-25 07:24:13 +0700

    Bound Service

--- app/src/main/java/com/dicoding/service/MainActivity.java
+++ app/src/main/java/com/dicoding/service/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dicoding.service;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +15,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnStartIntentService;
     private Button btnStartBoundService;
     private Button btnStopBoundService;
+
+    boolean mServiceBound = false;
+    BoundService mBoundService;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +63,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startService(mStartIntentService);
                 break;
             case R.id.btn_start_bound_service:
-
+                Intent mBoundServiceIntent = new Intent(MainActivity.this, BoundService.class);
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
                 break;
             case R.id.btn_stop_bound_service:
-
+                unbindService(mServiceConnection);
                 break;
 
         }
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+        }
+    }
+
 }
